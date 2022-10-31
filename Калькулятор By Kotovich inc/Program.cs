3ef422a88bfdf3e343891cfe5a1efb0533f214c3
KotovichTape <isip_d.d.kotovich@mpt.ru>
2022-10-31 10:29:37 +0300

    Калькулятор реальный

---
+++ Калькулятор By Kotovich inc/Program.cs
@@ -0,0 +1,118 @@
+﻿while (true)
+{
+    Console.Clear();
+    Console.WriteLine(
+    "Консольный калькулятор\n" +
+    "1. Сложение 2-х чисел\n" +
+    "2. Вычитание 2-х чисел\n" +
+    "3. Умножение 2-х чисел\n" +
+    "4. Деление двух чисел\n" +
+    "5. Возведение числа в пользовательскую степень\n" +
+    "6. Вычисление квадратного корня\n" +
+    "7. Вычисление 1% от числа\n" +
+    "8. Вычисление факториала\n" +
+    "9. Выход"
+    );
+    Console.Write("Ввод типа операции: ");
+    string o = Console.ReadLine();
+    string result = "0";
+    if (o == "1")
+    {
+        Console.WriteLine("Введите первое число: ");
+        double first_num = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите второе число: ");
+        double second_num = Convert.ToDouble(Console.ReadLine());
+        result = Convert.ToString(first_num + second_num);
+    }
+    else if (o == "2")
+    {
+        Console.WriteLine("Введите первое число: ");
+        double first_num = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите второе число: ");
+        double second_num = Convert.ToDouble(Console.ReadLine());
+        result = Convert.ToString(first_num - second_num);
+    }
+    else if (o == "3")
+    {
+        Console.WriteLine("Введите первое число: ");
+        double first_num = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите второе число: ");
+        double second_num = Convert.ToDouble(Console.ReadLine());
+        result = Convert.ToString(first_num * second_num);
+    }
+    else if (o == "4")
+    {
+        Console.WriteLine("Введите первое число: ");
+        double first_num = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите второе число: ");
+        double second_num = Convert.ToDouble(Console.ReadLine());
+        if (second_num == 0)
+        {
+            result = "Ошибка: нельзя делить на 0";
+        }
+        else
+        {
+            result = Convert.ToString(first_num / second_num);
+        }
+    }
+    else if (o == "5")
+    {
+        Console.Write("Введите первое число: ");
+        double first_num = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        double second_num = Convert.ToDouble(Console.ReadLine());
+        double raw_result = 1;
+        for (int i = 1; i <= second_num; i++)
+        {
+            raw_result *= first_num;
+        }
+        result = Convert.ToString(raw_result);
+    }
+    else if (o == "6")
+    {
+        Console.Write("Введите число: ");
+        double first_num = Convert.ToDouble(Console.ReadLine());
+        if (first_num < 0)
+        {
+            result = "Ошибка: нельзя считать корень отрицательного числа";
+        }
+        else
+        {
+            result = Convert.ToString(Math.Sqrt(first_num));
+        }
+    }
+    else if (o == "7")
+    {
+        Console.Write("Введите число: ");
+        double first_num = Convert.ToDouble(Console.ReadLine());
+        result = Convert.ToString(first_num / 100);
+    }
+    else if (o == "8")
+    {
+        Console.Write("Введите число: ");
+        int first_num = Convert.ToInt32(Console.ReadLine());
+        if (first_num < 1)
+        {
+            result = "Ошибка: нельзя вычислять факториал отрицательного числа";
+        }
+        else
+        {
+            int raw_result = 1;
+            for (int i = 1; i <= first_num; i++)
+            {
+                raw_result *= i;
+            }
+            result = Convert.ToString(raw_result);
+        }
+    }
+    else if (o == "9")
+    {
+        Environment.Exit(0);
+    }
+    else
+    {
+        result = "Неверная операция";
+    }
+    Console.WriteLine("\nРезультат работы программы: " + result + "\nНажмите любую клавишу для продолжения");
+    Console.ReadKey();
+}
